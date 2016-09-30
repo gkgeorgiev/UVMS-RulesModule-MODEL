693598965d7bda5e7834fbf5d810b8b322b77872
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-09-30 12:33:59 +0200

    Review by osdjup Build Dtos from model and change to releasebranch in poms

---
+++ src/main/java/org/w3/_2001/xmlschema/Adapter2.java
@@ -0,0 +1,20 @@
+
+package org.w3._2001.xmlschema;
+
+import java.util.Date;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class Adapter2
+    extends XmlAdapter<String, Date>
+{
+
+
+    public Date unmarshal(String value) {
+        return (eu.europa.ec.fisheries.uvms.rules.model.mapper.XsdDateTimeConverter.unmarshal(value));
+    }
+
+    public String marshal(Date value) {
+        return (eu.europa.ec.fisheries.uvms.rules.model.mapper.XsdDateTimeConverter.marshalDate(value));
+    }
+
+}
